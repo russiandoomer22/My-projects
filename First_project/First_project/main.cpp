6ee8e53dcfa38a279cb8db74c983fa0493772b78
Alexey Romashin <russiandoomer22@gmail.com>
2022-02-06 02:48:40 +0300

    Урок 5. Переменные

--- First_project/First_project/main.cpp
+++ First_project/First_project/main.cpp
@@ -1,28 +1,43 @@
-/*
-	\b
-
-	\n
-	\t
-
-	\\
-	\"
-	\'
-*/
+//5. Переменные
 
 #include <iostream>
 using namespace std;
 
 void main()
 {
-	setlocale(LC_ALL, "Rus"); // Функция, отвечающая за вывод русских символов в консоль
-	//cout << "Привет, мир!\n";
-	//cout << "Привет, мир!\n\n";
-	//cout << "Привет, мир!\t";
-	//cout << "Привет, мир!\t\t";
-	//cout << "\tПривет, мир!\t";
-	//cout << "\nПривет, мир!\n";
-	/*cout << "\"Привет, мир!\"\n";*/
-	//cout << "\"\'\\";
+	setlocale(LC_ALL, "Rus");
 
-	cout << "Привет. \nКак дела? \nПривет. \nОтлично.";
+	/*int a;
+	a = 5;
+	cout << a << endl;*/
+
+
+	/*int a = 5;
+	cout << a << endl;
+	a = 10;
+	cout << a << endl;*/
+
+	/*int a, b, c;
+	a = 1;
+	b = 2;
+	c = 3;
+	cout << a << " " << b << " " << c << endl;*/
+
+	/*int a = 1, b = 2, c = 3;
+	cout << a << " " << b << " " << c << endl;*/
+
+	/*int a;
+	cout << a << endl;*/
+
+	/*double a = 1.1;
+	int b = 2.2;
+	cout << a << "\n" << b << endl;*/
+
+	/*char a = 'А';
+	cout << a << endl;*/
+
+	bool a = true;
+	bool b = false;
+	cout << a << ' ' << b << endl;
+	cout << a << " " << b << endl;
 }
