e8ed3e50cd1c7f74891bfd67fe34a517ba2e9e57
Alexey Romashin <russiandoomer22@gmail.com>
2022-02-06 03:14:21 +0300

    Урок 6. Константы

--- First_project/First_project/main.cpp
+++ First_project/First_project/main.cpp
@@ -1,4 +1,4 @@
-//5. Переменные
+//6. Константы
 
 #include <iostream>
 using namespace std;
@@ -7,37 +7,13 @@ void main()
 {
 	setlocale(LC_ALL, "Rus");
 
-	/*int a;
-	a = 5;
-	cout << a << endl;*/
+	//const int COUNT_DAYS_IN_WEEK = 7;
+	//cout << COUNT_DAYS_IN_WEEK << endl;
+	////COUNT_DAYS_IN_WEEK = 5; // Ошибка
 
+	const char NEW_LINE = '\n';
+	cout << 1 << NEW_LINE << 2 << endl;
 
-	/*int a = 5;
-	cout << a << endl;
-	a = 10;
-	cout << a << endl;*/
-
-	/*int a, b, c;
-	a = 1;
-	b = 2;
-	c = 3;
-	cout << a << " " << b << " " << c << endl;*/
-
-	/*int a = 1, b = 2, c = 3;
-	cout << a << " " << b << " " << c << endl;*/
-
-	/*int a;
-	cout << a << endl;*/
-
-	/*double a = 1.1;
-	int b = 2.2;
-	cout << a << "\n" << b << endl;*/
-
-	/*char a = 'А';
-	cout << a << endl;*/
-
-	bool a = true;
-	bool b = false;
-	cout << a << ' ' << b << endl;
-	cout << a << " " << b << endl;
+	const char TAB = '\t';
+	cout << 3 << TAB << 4 << endl;
 }
